0446f37ff2c8eb3175c60ed4ccfd12ec1b0961a0
Viktor Zayats <zaychik89@ukr.net>
2019-06-24 15:00:21 +0300

    well done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from './modules/sum';
+import guard from './modules/guard';
+
+console.log(sum(5,22));
+console.log(guard('Viktor'));
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+export default function (name) {
+    if (name == 'Viktor') {
+        return 'hello, Viktor! Come in, please..';
+    }else {
+        return 'Fuck off!!!';
+    }
+};
